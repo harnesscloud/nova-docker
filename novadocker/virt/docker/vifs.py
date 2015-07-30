d2fe8c6dc60b894634f318970c84ac89ab97e87b
Mark Stillwell <mark@stillwell.me>
2015-07-30 10:07:03 +0100

    turns out, grid5000 needs mtu=1454...

--- novadocker/virt/docker/vifs.py
+++ novadocker/virt/docker/vifs.py
@@ -203,7 +203,7 @@ class DockerGenericVIFDriver(object):
                           'set', if_remote_name, 'address', vif['address'],
                           run_as_root=True)
             utils.execute('ip', 'netns', 'exec', container_id, 'ip', 'link',
-                          'set', 'dev', if_remote_name, 'mtu', 1476,
+                          'set', if_remote_name, 'mtu', 1454,
                           run_as_root=True)
             utils.execute('ip', 'netns', 'exec', container_id, 'ifconfig',
                           if_remote_name, ip, run_as_root=True)
